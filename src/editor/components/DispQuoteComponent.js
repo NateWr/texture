f04efd9f9a8b1f6a1ea24756cf64d2c2f92944d6
Nate Wright <natew@notthisway.com>
2017-08-03 21:03:32 +0100

    Use semantic HTML tags for disp-quote component

--- src/editor/components/DispQuoteComponent.js
+++ src/editor/components/DispQuoteComponent.js
@@ -1,12 +1,13 @@
 import { Component, TextPropertyEditor } from 'substance'
 
 export default class DispQuote extends Component {
+
   render($$) {
     const node = this.props.node
     let quoteContent = node.find('disp-quote-content')
     let attribContent = node.find('attrib')
-    
-    let el = $$('div')
+
+    let el = $$('blockquote')
       .addClass('sc-disp-quote')
       .attr('data-id', node.id)
 
@@ -18,9 +19,10 @@ export default class DispQuote extends Component {
     let attribContentEl = $$(TextPropertyEditor, {
       placeholder: 'Enter attribution',
       path: attribContent.getTextPath(),
-      disabled: this.props.disabled
+      disabled: this.props.disabled,
+      tagName: 'cite'
     }).addClass('se-attribution')
-    
+
     el.append(
       quoteContentEl,
       attribContentEl
